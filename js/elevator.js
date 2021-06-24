f7c6e61b322d0774d65b273972197cf47e564e59
Валерий <rasvv@mail.ru>
2021-06-24 23:43:57 +0300

    Добавил шрифт

---
+++ js/elevator.js
@@ -0,0 +1,37 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.elevator__container')
+const slidesCount = mainSlide.children.length
+
+const heightScreen = container.clientHeight 
+let activeSlide = 0
+sidebar.style.top = `-${(slidesCount - 1) * 500}px`
+
+const changeSlide = (direction) => {
+	if (direction === 'up') {
+		activeSlide++
+		if (activeSlide === slidesCount) {
+			activeSlide = 0
+		}
+	}
+	if (direction === 'down') {
+		activeSlide--
+		if (activeSlide < 0) {
+			activeSlide = slidesCount - 1
+		}
+	}
+
+	mainSlide.style.transform = `translateY(-${heightScreen * activeSlide}px)`
+	sidebar.style.transform = `translateY(${heightScreen * activeSlide}px)`
+
+}
+
+upBtn.addEventListener('click', () => {
+	changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+	changeSlide('down')
+})
